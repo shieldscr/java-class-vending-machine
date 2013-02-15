01f63ab476fe40184b45e86b9013851c98af3ef4
Guy Royse <guy@guyroyse.com>
2013-02-14 22:41:04 -0500

    removed hacks to redirect to servlet

--- VendingMachineWeb/src/java/com/awesomevendingco/vendingmachine/ui/BootMachineServlet.java
+++ VendingMachineWeb/src/java/com/awesomevendingco/vendingmachine/ui/BootMachineServlet.java
@@ -2,9 +2,7 @@ package com.awesomevendingco.vendingmachine.ui;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -13,18 +11,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.awesomevendingco.vendingmachine.machine.VendingMachine;
 
-public class BootMachineServlet extends HttpServlet {
+public class BootMachineServlet extends HelpfulServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
         VendingMachine machine = (VendingMachine) context.getBean("machine");
 
-        request.getSession().setAttribute("machine", machine);
+        setApplicationScope(getServletContext(), "machine", machine);
 
-        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/machine.jsp");
-        dispatcher.forward(request, response);
-
+        forwardToPage(request, response, "/machine.jsp");
     }
 
 }
